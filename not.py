adcdefdd8dd2b572efa19786835d1c8d758cfccd
SaatvikLodha <saatviklodha@gmail.com>
2026-06-19 15:42:38 -0500

    Add files via upload

---
+++ not.py
@@ -0,0 +1,19 @@
+a=10
+b=12
+c=12
+print(not(a==b))
+
+x="python"
+d="coding"
+if not x==d:
+    print(not(x==d))
+
+q=25
+b=50
+if not q==b:
+    print(not(q==b))
+
+s=10
+v=10
+if(not(s==v)):
+    print(not(s==v))
