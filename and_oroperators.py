941ff1195a93e2e41c1835a79f4ef73e635c26dd
SaatvikLodha <saatviklodha@gmail.com>
2026-06-19 15:19:20 -0500

    Add files via upload

---
+++ and_oroperators.py
@@ -0,0 +1,20 @@
+a=5
+b=7
+c=0
+if a and b and c:
+    print(True)
+else:
+    print("At least one of the numbers is",False)
+
+x=10
+y=-10
+z=0
+if x or y or z>0:
+    print("At least one of the numbers is greater than 0")
+else:
+    print("None of the numbers are greater than 0")
+
+if a or b or c>0:
+    print("At least one of the numbers is greater than 0")
+else:
+    print("None of the numbers are greater than 0")
